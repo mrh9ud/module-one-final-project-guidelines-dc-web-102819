80e80a2921ab05e027ec4a4d5e67e25fe8de5837
mrh9ud <mrh9ud@virginia.edu>
2019-11-12 17:53:58 -0500

    submenus navigable

--- bin/methods.rb
+++ bin/methods.rb
@@ -28,14 +28,58 @@ class CLI
     end
 
     def main_menu
-        main_menu = PROMPT.select("#{PROGRAM_NAME} Main Menu:") do |menu|
+        @@main_menu_selection = PROMPT.select("#{PROGRAM_NAME} Main Menu:") do |menu|
             menu.choice "Tags"
             menu.choice "Questions"
             menu.choice "Answers"
             menu.choice "Date Posted"
         end
+        sub_menu_case_selector
+        # binding.pry
     end
 
+    def sub_menu_case_selector
+        case @@main_menu_selection
+            when @@main_menu_selection = "Tags"
+                tag_menu
+            when @@main_menu_selection = "Questions"
+                question_menu
+            when @@main_menu_selection = "Answers"
+                answer_menu
+            when @@main_menu_selection = "Date Posted"
+                date_posted_menu
+            end
+    end
+        
+    def tag_menu
+        tag_menu = PROMPT.select("Tag Menu:") do |tag_menu|
+            tag_menu.choice "Most Common Tags"
+            tag_menu.choice "Tags Listed by Frequency of Use"
+            tag_menu.choice "Sort Answers by Tags"
+            tag_menu.choice "Sort Questions by Tags"
+        end
+    end
+
+    def question_menu
+        question_menu = PROMPT.select("Question Menu:") do |question_menu|
+            question_menu.choice
+            question_menu.choice
+        end
+    end
+
+    def answer_menu
+        answer_menu = PROMPT.select("Answer Menu:") do |answer_menu|
+            answer_menu.choice
+            answer_menu.choice
+        end
+    end
+
+    def date_posted_menu
+        date_posted_menu = PROMPT.select("Date Posted Menu:") do |date_posted_menu|
+            date_posted.choice
+            date_posted.choice
+        end
+    end
     # def most_common_tags
     #     sql = <<-SQL
     #         SELECT tags.name, COUNT(question_tags.tag_id) 
--- lib/tag.rb
+++ lib/tag.rb
@@ -1,3 +1,8 @@
 class Tag < ActiveRecord::Base
     has_many :question_tags
+
+    def self.most_common_tags
+        
+    end
+
 end
