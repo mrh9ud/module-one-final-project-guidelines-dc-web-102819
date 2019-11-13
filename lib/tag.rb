5752e1140656df4a1a94a1acbc0ba3701dd32be9
mrh9ud <mrh9ud@virginia.edu>
2019-11-12 20:22:03 -0500

    sub menu templates

--- lib/tag.rb
+++ lib/tag.rb
@@ -5,4 +5,26 @@ class Tag < ActiveRecord::Base
         
     end
 
+    def self.most_frequent_tags
+
+    end
+
+    def self.answers_sorted_by_tags
+
+    end
+
+    def self.questions_sorted_by_tags
+
+    end
 end
+
+    # def most_common_tags
+    #     sql = <<-SQL
+    #         SELECT tags.name, COUNT(question_tags.tag_id) 
+    #         FROM question_tags 
+    #         INNER JOIN tags ON 
+    #         question_tags.tag_id = tags.id 
+    #         GROUP BY question_tags.tag_id LIMIT 2
+    #     SQL
+    #     DB[:conn].execute(sql)
+    # end
